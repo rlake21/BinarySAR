1a8e9d4aa2955e778d38bd8e46daa274879dfb71
Ryan Lake <rlake@mines.edu>
2014-04-12 16:10:00 -0600

    added airTeam, dogTeam and groundTeam classes with empty methods

--- src/SAR/SearchTeam.java
+++ src/SAR/SearchTeam.java
@@ -18,4 +18,44 @@ public abstract class SearchTeam {
 		
 	}
 
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(int yPos) {
+		this.yPos = yPos;
+	}
+
+	public double getxVelocity() {
+		return xVelocity;
+	}
+
+	public void setxVelocity(double xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+
+	public double getyVelocity() {
+		return yVelocity;
+	}
+
+	public void setyVelocity(double yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+
 }
